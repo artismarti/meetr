a9d4d5ac8fd1db19ba720ff8a0c0fe63dd4959f4
artismarti <artismarti@gmail.com>
2018-12-05 09:54:45 +0000

    Updates & Creates

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -14,9 +14,14 @@ class MeetingsController < ApplicationController
   end
 
   def create
-    @meeting = Meeting.create(meeting_params(:title, :date_time))
+    @users = User.all
+    @meeting = Meeting.new(meeting_params(:title, :date_time))
     if @meeting.valid?
       @meeting.save
+      UserMeeting.create!(:user_id => params[:meeting][:creator_id], :meeting_id => @meeting.id, :user_status => "created")
+      params[:meeting][:invitee_ids].each do |i|
+        UserMeeting.create(:user_id => i, :meeting_id => @meeting.id, :user_status => "invited")
+      end
       redirect_to meeting_path(@meeting)
     else
       flash[:errors] = @meeting.errors.full_messages
@@ -25,12 +30,17 @@ class MeetingsController < ApplicationController
   end
 
   def edit
+    @users = User.all
   end
 
   def update
     @meeting.update(meeting_params(:title, :date_time))
     if @meeting.valid?
       @meeting.save
+      UserMeeting.update(:user_id => params[:meeting][:creator_id], :meeting_id => @meeting.id, :user_status => "created")
+      params[:meeting][:invitee_ids].each do |i|
+        UserMeeting.update(:user_id => i, :meeting_id => @meeting.id, :user_status => "invited")
+      end
       redirect_to meeting_path(@meeting)
     else
       flash[:errors] = @meeting.errors.full_messages
@@ -52,5 +62,11 @@ class MeetingsController < ApplicationController
   def meeting_params(*args)
     params.require(:meeting).permit(*args)
   end
+  # def creator_params
+  #   params.require(:meeting).permit(:creator_id)
+  # end
+  # def invitee_params
+  #   params.require(:meeting).permit(:invitee_id)
+  # end
 
 end
