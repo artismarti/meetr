2e6f9f8f13afe214232bc7fb4e4e3bf4d4595cd0
artismarti <artismarti@gmail.com>
2018-12-05 09:56:48 +0000

    All other updates. I can’t write more descriptive commit messages anymore. I’m tired.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :user_meetings, :dependent => :delete_all
   has_many :meetings, through: :user_meetings
+  has_secure_password
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :user_meetings
   resources :meetings
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/sessions', to: 'sessions#create', as: 'sessions'
+  post '/logout', to: 'sessions#destroy', as: 'logout'
+
 end
